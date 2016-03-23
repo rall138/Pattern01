a6f35941370a3472e50b01f5454e9795402acfc5
Ricardo Lomez <rlomez@FRANQICURU.LOCAL>
2016-03-23 17:51:05 -0300

    Comienzo de generación de treeview

--- src/pattern01/PatternNav.java
+++ src/pattern01/PatternNav.java
@@ -1,42 +1,99 @@
 package pattern01;
 
-import java.net.MalformedURLException;
 import java.net.URL;
-
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
+import org.osgi.framework.Bundle;
 
 public class PatternNav extends ViewPart{
 
-	Action searchPatternAction;
+	private Action searchPatternAction;
+	private Composite mParent;
 	
 	@Override
 	public void createPartControl(Composite parent) {
+		mParent = parent;
 		searchPatternAction = new Action("search..") {
 			public void run(){
 				//Aca va el código de la acción
 			}
 		};
-		searchPatternAction.setImageDescriptor(getImageDescriptor("linux.png"));
-		
-		
-		Label label = new Label(parent, 0);
-		label.setText("Nodo padre de view");
-	
+		searchPatternAction.setImageDescriptor(getImageDescriptor("lupa.png"));
+		createActionBar();
+
+		Tree tree = new Tree(parent, SWT.H_SCROLL | SWT.V_SCROLL);
+		TreeItem item = new TreeItem(tree, 0);
+		Image image = getImage("package_obj.png");
+		item.setImage(image);
+		item.setText("Entidad de prueba");
+		TreeItem sub_item = new TreeItem(item, 0);
+		image = getImage("class_obj.png");		
+		sub_item.setImage(image);
+		sub_item.setText("Sub item");
+		image = getImage("ww.ico");
+		TreeItem sub_item2 = new TreeItem(sub_item, 0);
+		sub_item2.setImage(image);
+		sub_item2.setText("Sub item 2");
+		image = getImage("primefaces.ico");		
+		TreeItem sub_item3 = new TreeItem(sub_item2, 0);
+		sub_item3.setImage(image);
+		sub_item3.setText("Sub item 3");		
 	}
 	
-	private ImageDescriptor getImageDescriptor(String path){
-		String iconPath= "src\\icons\\";
-
-		try{
-			return ImageDescriptor.createFromURL(new URL(iconPath + path));
-		}catch(MalformedURLException ex){
-			ex.printStackTrace(System.err);
-		}
-		return null;
+	private ImageDescriptor getImageDescriptor(String imageName){
+		String iconPath = "icons/";
+		Bundle bundle = Platform.getBundle("Pattern01");
+		Path path = new Path(iconPath + imageName);
+		URL fileUrl = FileLocator.find(bundle, path, null);
+		return ImageDescriptor.createFromURL(fileUrl);
+	}
+	
+	private Image getImage(String imageName){
+		return ImageDescriptor.createFromURL(getClass().getResource("../icons/" + imageName)).createImage();
+	}
+	
+	private void createActionBar(){
+		IToolBarManager mgr = getViewSite().getActionBars().getToolBarManager();
+		mgr.add(searchPatternAction);
+	}
+	
+	private void setSelectionListener(){
+		ISelectionListener listener = new ISelectionListener() {
+			
+			@Override
+			public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+				
+				
+			}
+		};
+	}
+	
+	private void setSecundaryClickListener(){
+		MenuManager mgr = new MenuManager();
+		mgr.addMenuListener(new IMenuListener() {
+			
+			@Override
+			public void menuAboutToShow(IMenuManager manager) {
+			
+				
+			}
+		});
 	}
 
 	@Override
