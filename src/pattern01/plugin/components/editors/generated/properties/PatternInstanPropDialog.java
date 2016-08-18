240fcb45af9d3450fd26bbfa7803b9ea41d85916
= <ricardo.lomez@gmail.com>
2016-08-18 08:58:01 -0300

    Cambios menores

---
+++ src/pattern01/plugin/components/editors/generated/properties/PatternInstanPropDialog.java
@@ -0,0 +1,30 @@
+package pattern01.plugin.components.editors.generated.properties;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+public class PatternInstanPropDialog extends TitleAreaDialog{
+
+	public PatternInstanPropDialog(Shell parentShell) {
+		super(parentShell);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		super.create();
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		// TODO Auto-generated method stub
+		return super.createDialogArea(parent);
+	}
+
+
+	
+	
+}
